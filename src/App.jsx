7a9872ca6fdc0edc14eb2d53f42427a61de35d0d
juanlopcon07 <juan.21futmas07@gmail.com>
2025-04-28 21:32:10 -0500

    Componentes reutilizables con props y estado local en react

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import './App.css'
+import Card from './componentes/Card';
+import Counter from './componentes/Counter';
+
+function App() {
+  const items = ["React", "JavaScript", "Vite"];
+
+  return (
+    <section>
+      <h1>¡Hola Mundo!</h1>
+      <Card title="Card 1" description="Esta es una descripcion"/>
+      <Card title="Card 2" description="Esta es otra descripcion"/>
+      <ul>
+        {
+          items.map((item, index) => (
+            <li key={index}>{item}</li>
+          ))
+        }
+      </ul>
+      <Counter/>
+    </section>
+  )
+}
+
+export default App
