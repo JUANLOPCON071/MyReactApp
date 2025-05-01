b2bd6f034b4f8c6f12e80eaac83ff7ea0c973eb5
juanlopcon07 <juan.21futmas07@gmail.com>
2025-05-01 17:27:12 -0500

    Estilos en React: CSS, SaaS y Modulos CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import Card from './componentes/Card';
+import Card from './componentes/Card/Card';
 import Counter from './componentes/Counter';
 import CounterWhithEffect from './componentes/CounterWithEffect';
 import NameForm from './componentes/NameForm';
@@ -11,18 +11,7 @@ function App() {
   return (
     <section>
       <h1>Formulario de nombre</h1>
-      {/* <Card title="Card 1" description="Esta es una descripcion"/>
-      <Card title="Card 2" description="Esta es otra descripcion"/>
-      <ul>
-        {
-          items.map((item, index) => (
-            <li key={index}>{item}</li>
-          ))
-        }
-      </ul>
-      <Counter/>
-      <ToggleButton/> */}
-      {/* <NameForm/> */}
+      <Card/>
       <CounterWhithEffect/>
     </section>
   )
---
+++ src/componentes/Card/Card.jsx
@@ -0,0 +1,12 @@
+// import "./styles.css"
+import styles from "./Card.module.sass"
+
+const Card = () => {
+    return (
+        <div className={styles.card}>
+            Soy una card!
+        </div>
+    )
+}
+
+export default Card;
