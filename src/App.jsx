814b7be08b19fb7ad5c363242602764f5d3660c4
juanlopcon07 <juan.21futmas07@gmail.com>
2025-05-03 18:35:47 -0500

    Optimizacion de fetch con useEffect y dependencias en react

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Card from './componentes/Card/Card';
 import Counter from './componentes/Counter';
 import CounterWhithEffect from './componentes/CounterWithEffect';
 import NameForm from './componentes/NameForm';
+import SearchPosts from './componentes/SearchPosts/SearchPosts';
 import StaticComponent from './componentes/StaticComponent/StaticComponent';
 import TailwindText from './componentes/Text/Text';
 import ToggleButton from './componentes/ToggleButton';
@@ -15,9 +16,7 @@ function App() {
 
   return (
     <>
-      <StaticComponent/>
-      {/* <UserList/> */}
-      <UserListWithLoading/>
+      <SearchPosts/>
     </>
   )
 }
---
+++ src/componentes/SearchPosts/SearchPosts.jsx
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react";
+
+const SearchPosts = () => {
+    const [query, setQuery] = useState("");
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        fetch(`https://jsonplaceholder.typicode.com/posts?title_like=${query}`)
+        .then((response) => response.json())
+        .then((data) => setPosts(data))
+        .catch((error) => console.error("Error fetching data: ", error))
+    }, [query])
+
+    return (
+        <div>
+            <h1>Lista de Usuarios</h1>
+            <input 
+                type="text"
+                placeholder="Buscar por titulo"
+                value={query}
+                onChange={(event) => setQuery(event.target.value)} 
+            />
+            <ul>
+                {
+                    posts.map((post) => (
+                        <>
+                            <li key={post.id}>{post.title}</li>
+                        </>
+                    ))
+                }
+            </ul>
+        </div>
+    )
+}
+
+export default SearchPosts;
